d76cde6fc54244f0fc5f7203946f419ecb0bf961
ESTSW-EQPMNT-NB-005 <kimjs3061@naver.com>
2022-01-04 09:12:51 +0900

    회원 웹 기능 - 조회 (thymeleaf)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.Service.MemberService;
 import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -40,5 +43,10 @@ public class MemberController {
         return "redirect:/";
     }
 
-
+    @GetMapping("/members")
+    public String List(Model model) {
+        List<Member> members = memberService.findmembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
